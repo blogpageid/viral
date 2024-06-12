dcc8db974ea073f09b5383f002e76d4466b92799
blogpageid <blog.page.id@gmail.com>
2024-06-13 02:51:08 +0700

    Update ads.tsx

--- components/ads.tsx
+++ components/ads.tsx
@@ -1,7 +1,7 @@
 import Script from 'next/script';
 
 const AdScript = () => (
-  <Script src="\/\/februaryinjectionproudly.com\/75\/71\/14\/7571143e30fdf9eee201ee7d5c03c91e.js"/>
+  <Script src="\/\/pl23426081.highrevenuenetwork.com\/58\/a5\/8c\/58a58c48f27435dc062154e664b37b6e.js"/>
 );
 
 export default AdScript;
